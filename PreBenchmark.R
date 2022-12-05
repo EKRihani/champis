ab22a85d89599f5677db061920e71c9a572fa5a6
ekrihani <emir.kais@rihani.eu>
2022-12-05 20:46:09 +0100

    05dec 20:45

--- PreBenchmark.R
+++ PreBenchmark.R
@@ -2,12 +2,14 @@ library(tidyverse)   # Trousse à outils générique
 library(microbenchmark)   # Benchmark de fonctions
 #library(caret)       # Apprentissage machine
 library(DataExplorer)   # Outils pour EDA
-library(FactoMineR)
+#library(FactoMineR)
 # library(factoextra)
 
-N_diamant <- 2e2   # Facteur de répétition
+######################
+#  DATASET DIAMONDS  #
+######################
+N_diamant <- 5e0   # Facteur de répétition
 sd_diamant <- .02  # Facteur de dispersion
-
 diamants <- diamonds %>% slice(rep(1:n(), each = N_diamant)) # Répétition des données
 
 # Appliquer un facteur aléatoire aux valeurs numériques
@@ -20,18 +22,35 @@ for(i in 1:length(col_num_diamant))
   }
 
 
-MB_graphes_diamant <- microbenchmark(
-  UN = plot(x = diamants[1:1e1,]$x, y = diamants[1:1e1,]$y),
-  DEUX = plot(x = diamants[1:1e2,]$x, y = diamants[1:1e2,]$y),
-  TROIS = plot(x = diamants[1:1e3,]$x, y = diamants[1:1e3,]$y),
-  QUATRE = plot(x = diamants[1:1e4,]$x, y = diamants[1:1e4,]$y),
-  CINQ = plot(x = diamants[1:1e5,]$x, y = diamants[1:1e5,]$y),
-  SIX = plot(x = diamants[1:1e6,]$x, y = diamants[1:1e6,]$y),
-  times = 30,
-  unit = "s"
-)
-microbenchmark:::boxplot.microbenchmark(MB_graphes_diamant, unit = "ms", log = FALSE)
 
+#######################
+#  DATASET MUSHROOMS  #
+#######################
+URL <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00615/MushroomDataset.zip"
+datafile <- tempfile()
+download.file(URL, datafile)
+datafile <- unzip(datafile, "MushroomDataset/secondary_data.csv")
+shrooms <- read.csv(datafile, header = TRUE, sep = ";")
+
+N_shroomz <- 100   # Facteur de répétition
+sd_shroomz <- .02  # Facteur de dispersion
+shroomz <- shrooms %>% slice(rep(1:n(), each = N_shroomz)) # Répétition des données
+
+# Appliquer un facteur aléatoire aux valeurs numériques
+col_num_shroomz <- names(select_if(shroomz, is.numeric))
+for(i in 1:length(col_num_shroomz))
+{
+   ma_colonne <- paste0("shroomz$", col_num_diamant[i])
+   ma_commande <- paste0(ma_colonne, "<-", ma_colonne, "* rnorm(n = nrow(shroomz), mean = 1, sd = sd_shroomz)")
+   eval(parse(text = ma_commande))
+}
+
+##############
+#  ANALYSES  #
+##############
+
+create_report(data = shroomz, y = "class")
+plot(diamants$x, diamants$y)
 MB_EDA_diamant <- microbenchmark(
    # UN = create_report(data = diamants[1:1e1,], y = "price"),
    # DEUX = create_report(data = diamants[1:1e2,], y = "price"),
@@ -43,9 +62,11 @@ MB_EDA_diamant <- microbenchmark(
    times = 2,
    unit = "s"
 )
-
+create_report(data = diamants, y = "price")
 microbenchmark:::boxplot.microbenchmark(MB_EDA_diamant, unit = "s", log = TRUE)
    
+
+
 r_pca <- FactoMineR::PCA(df)
 r_ca <- FactoMineR::CA(df)
 r_pca$eig
