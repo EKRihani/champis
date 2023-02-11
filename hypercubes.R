ab850e3f9d75beb0e71dc23b67bdd64a5935e067
EKRihani <emir.kais@rihani.eu>
2023-02-11 22:17:29 +0100

    11fev 22:20

---
+++ hypercubes.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(lhs)
+
+geneticLHS(n = 10, k = 3, pop = 50, gen = 2, criterium = "Maximin")
+
+
+library(DiceDesign)
+LHS <- lhsDesign(n = 15, dimension = 3, randomized=TRUE, seed=13)$design
+opti_LHS <- maximinESE_LHS(LHS)$design
+plot(opti_LHS)
+
+
+LIN <- function(x, y, z){30*x+ 25*y - 12*z}
+BILIN <- function(x, y, z){30*x+ 25*y - 12*z + 17*x*y - 3*x*z + 8*y*z}
+QUAD <- function(x, y, z){30*x+ 25*y - 12*z + 7*x^2 + 15*y^2 - 5*z^2}
+BIQUAD <- function(x, y, z){30*x+ 25*y - 12*z + 17*x*y - 3*x*z + 8*y*z + 7*x^2 + 15*y^2 - 5*z^2}
+lin <- LIN(opti_LHS[,1], opti_LHS[,2], opti_LHS[,3])
+bilin <- BILIN(opti_LHS[,1], opti_LHS[,2], opti_LHS[,3])
+quad <- QUAD(opti_LHS[,1], opti_LHS[,2], opti_LHS[,3])
+biquad <- BIQUAD(opti_LHS[,1], opti_LHS[,2], opti_LHS[,3])
+
+library(DiceEval)
+modelComparison(X=opti_LHS,Y=quad,type="all", formula=Y~X1+X2+X1:X2+I(X1^2)+I(X2^2))
+
+Modele_Mars <- modelFit(X=opti_LHS, Y=biquad, type="MARS", degree=4)
+Modele_PolyMars <- modelFit(X=opti_LHS, Y=biquad, type="PolyMARS", gcv=4)
+Modele_Kriging <- modelFit(X=opti_LHS, Y=biquad, type="Kriging", formula=Y~X1+X2+X3+X1:X2+X1:X3+X2:X3+I(X1^2)+I(X2^2)+I(X3^2))
+
+
+Test <- BIQUAD(LHS[,1], LHS[,2], LHS[,3])
+Pred_Mars <- modelPredict(Modele_Mars, LHS)
+Pred_PolyMars <- modelPredict(Modele_PolyMars, LHS)
+Pred_Kriging <- modelPredict(Modele_Kriging, LHS)
+
+
+library(DiceKriging)
+KM <- km(design=opti_LHS, response=biquad)
+predict(object=KM, newdata=LHS, type="UK")
+
+
+data.frame(Test, Pred_Mars, Pred_PolyMars, Pred_Kriging)
--- introduction.R
+++ introduction.R
@@ -12,70 +12,76 @@ library(plotly)   # Graphes avancés
 #       FACTEUR CROISSANCE       #
 ##################################
 
-n_distrib <- 1e6
+n_chrono <- 1e6   # Nombre de valeurs pour microbenchmark
+n_graph <- 1e5    # Nombre de valeurs pour graphiques
 
 # Lois de distribution
 
 binom_size <- 20
-fonction_binomiale <- function(x){rbinom(n = n_distrib, size = binom_size, prob = .65)/binom_size}
-temps_binomiale <- microbenchmark(fonction_binomiale(x))
-valeurs_binomiale <- data.frame(val = fonction_binomiale(x))
+fonction_binomiale <- function(x,N){rbinom(n = N, size = binom_size, prob = .65)}
+temps_binomiale <- microbenchmark(fonction_binomiale(x, n_chrono))
+valeurs_binomiale <- data.frame(val = fonction_binomiale(x, n_graph))
 distrib_binomiale <- ggplot(data = valeurs_binomiale, aes(x = val)) +
-  ggtitle("Loi binomiale") +
+#  ggtitle("Loi binomiale") +
 #  xlim(min =0, max = 1) +
   xlab("Valeur") +
-  ylab("Densité") +
-  geom_histogram(fill = "grey", color= "black", bins = 4*binom_size, center = 0.5) + 
-  theme_bw()
+  ylab("") +
+  geom_histogram(fill = "grey40", bins = 4*binom_size, center = 0.5) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(angle=90, vjust=.5, hjust=.5))
 
-fonction_uniforme <- function(x){runif(n = n_distrib)}
-temps_uniforme <- microbenchmark(fonction_uniforme(x))
-valeurs_uniforme <- data.frame(val = fonction_uniforme(x))
+fonction_uniforme <- function(x,N){runif(n = N)}
+temps_uniforme <- microbenchmark(fonction_uniforme(x, n_chrono))
+valeurs_uniforme <- data.frame(val = fonction_uniforme(x, n_graph))
 distrib_uniforme <- ggplot(data = valeurs_uniforme, aes(x = val)) +
-  ggtitle("Loi uniforme") +
+#  ggtitle("Loi uniforme") +
   xlim(min =0, max = 1) +
   xlab("Valeur") +
-  ylab("Densité") +
-  geom_histogram(fill = "grey", color= "black", bins = 4*binom_size, center = 0.5) + 
-  theme_bw()
+  ylab("") +
+  geom_histogram(fill = "grey40", bins = 4*binom_size, center = 0.5) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(angle=90, vjust=.5, hjust=.5))
 
-fonction_normale <- function(x){rnorm(n = n_distrib)}
-temps_normale <- microbenchmark(fonction_normale(x))
-valeurs_normale <- data.frame(val = fonction_normale(x))
+fonction_normale <- function(x,N){rnorm(n = N)}
+temps_normale <- microbenchmark(fonction_normale(x, n_chrono))
+valeurs_normale <- data.frame(val = fonction_normale(x, n_graph))
 distrib_normale <- ggplot(data = valeurs_normale, aes(x = val)) +
-  ggtitle("Loi normale") +
+#  ggtitle("Loi normale") +
 #  xlim(min =0, max = 1) +
   xlab("Valeur") +
-  ylab("Densité") +
-  geom_histogram(fill = "grey", color= "black", bins = 4*binom_size, center = 0.5) + 
-  theme_bw()
+  ylab("") +
+  geom_histogram(fill = "grey40", bins = 4*binom_size, center = 0.5) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(angle=90, vjust=.5, hjust=.5))
 
-fonction_beta <- function(x){rbeta(n = n_distrib, shape1 = 6, shape2 = 4, ncp = .5)}
-temps_beta <- microbenchmark(fonction_beta(x))
-valeurs_beta <- data.frame(val = fonction_beta(x))
+fonction_beta <- function(x,N){rbeta(n = N, shape1 = 6, shape2 = 4, ncp = .5)}
+temps_beta <- microbenchmark(fonction_beta(x, n_chrono))
+valeurs_beta <- data.frame(val = fonction_beta(x, n_graph))
 distrib_beta <- ggplot(data = valeurs_beta, aes(x = val)) +
-  ggtitle("Loi beta") +
+#  ggtitle("Loi beta") +
   xlim(min =0, max = 1) +
   xlab("Valeur") +
-  ylab("Densité") +
-  geom_histogram(fill = "grey", color= "black", bins = 2*binom_size, center = 0.5) + 
-  theme_bw()
+  ylab("") +
+  geom_histogram(fill = "grey40", bins = 2*binom_size, center = 0.5) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(angle=90, vjust=.5, hjust=.5))
 
-fonction_poisson <- function(x){rpois(n = n_distrib, lambda = binom_size/3)/binom_size}
-temps_poisson <- microbenchmark(fonction_poisson(x))
-valeurs_poisson <- data.frame(val = fonction_poisson(x))
+fonction_poisson <- function(x,N){rpois(n = N, lambda = binom_size/3)/binom_size}
+temps_poisson <- microbenchmark(fonction_poisson(x, n_chrono))
+valeurs_poisson <- data.frame(val = fonction_poisson(x, n_graph))
 distrib_poisson <- ggplot(data = valeurs_poisson, aes(x = val)) +
-  ggtitle("Loi de Poisson") +
+#  ggtitle("Loi de Poisson") +
   xlim(min =0, max = 1) +
   xlab("Valeur") +
-  ylab("Densité") +
-  geom_histogram(fill = "grey", color= "black", center = 0.5, bins = 4*binom_size) + 
-  theme_bw()
+  ylab("") +
+  geom_histogram(fill = "grey40", center = 0.5, bins = 4*binom_size) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(angle=90, vjust=.5, hjust=.5))
 
 # Focus sur la loi Beta pour génération champis
 facteurs <- c(.5, 1, 1.5, 2)
 
-loi_beta <- function(facteur){rbeta(n = n_distrib, shape1 = 6*facteur, shape2 = 4, ncp = .5*facteur)}
+loi_beta <- function(facteur){rbeta(n = n_graph, shape1 = 6*facteur, shape2 = 4, ncp = .5*facteur)}
 
 beta <-lapply(X = facteurs, FUN = loi_beta)
 names(beta) <- facteurs
@@ -84,8 +90,8 @@ beta <- pivot_longer(beta, cols = 1:ncol(beta))
 beta$name <- str_remove(beta$name, "X")
 
 lois_beta <- ggplot(data = beta, aes(x = value, colour = name)) +
-  ggtitle("Distribution de différentes lois beta") +
-  labs(colour= "Fact. crois.") +
+#  ggtitle("Distribution de différentes lois beta") +
+  labs(colour= "Fc") +
   xlab("Valeur") +
   ylab("Densité") +
   geom_density(alpha = .7, bw = .025, size = .6) + 
